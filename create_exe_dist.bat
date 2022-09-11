2a2445d461903b91f2e2eaa88f91ebb050f4fad1
DESKTOP\randy <randygeraldbrooks2@gmail.com>
2022-09-11 13:47:36 -0400

    clean up dist script

--- create_exe_dist.bat
+++ create_exe_dist.bat
@@ -4,10 +4,13 @@ echo SRRebootTools%ver%
 mkdir ..\SRRebootTools%ver%
 xcopy /e . ..\SRRebootTools%ver%
 cd ..\SRRebootTools%ver%
-pyinstaller src/gui.py
+pyinstaller src/app/gui.py
+python3 -Bc "import pathlib; [p.unlink() for p in pathlib.Path('.').rglob('*.py[co]')]"
+python3 -Bc "import pathlib; [p.rmdir() for p in pathlib.Path('.').rglob('__pycache__')]"
 rd /s /q build
 robocopy /move /e dist\gui .
 rd /s /q dist
+robocopy /e src\dist dist
 move gui.exe SRRebootTools%ver%.exe
 rd /s /q .vscode
 powershell Compress-Archive . SRRebootTools%ver%.zip
